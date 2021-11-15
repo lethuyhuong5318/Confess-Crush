286b9f38013e73dfdc01c443f0c5a761382f574e
lethuyhuong5318 <94363316+lethuyhuong5318@users.noreply.github.com>
2021-11-15 18:16:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    titleWeb: "Minh à...:))))",
+    introTitle: 'Cuối cấp rồi...',
+    introDesc: `Tao chỉ muốn nói là tao...ĐÉO THÍCH MÀY`,
+    btnIntro: '^^Mà tao thương mày .....',
+    title: 'Tao ghét mày vãi..Cho đến khi thấy mày khóc bruhhhhhhh',
+    desc: 'Mày có thích tao ko. Dm xin 2p tự kỉ :))) ',
+    btnYes: 'Có',
+    btnNo: 'Đéo mày',
+    question: 'Ơ kìa sao lại thích tao zãy??',
+    btnReply: 'dm biết khùng rồi mà cái code nó như zãy huhu',
+    reply: 'Tao thích mày ko cần lí doo ok',
+    mess: 'I know :))))). Iu nhắm cơ',
+    messDesc: 'CN coi phim koooooo.',
+    btnAccept: 'Ok',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
